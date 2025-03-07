e4fb5ed21e77e3c873197edd8c3ce769dd711b44
lucas <lucascaro97@gmail.com>
2025-03-06 21:48:34 -0300

    refactoring proyect to inject local database and organize the repository

---
+++ CurrencyConverter/src/main/java/com/converter/CurrencyConverter/CurrencyConverterApplication.java
@@ -0,0 +1,16 @@
+package com.converter.CurrencyConverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class CurrencyConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterApplication.class, args);
+	}
+
+}
---
+++ conversor-front-app/src/components/SelectCurrency.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react"
+import axios from 'axios'
+
+
+export default function SelectCurrency({textLabel, className, onSelect}) {
+  
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+
+  const handleChange = (event) => {
+    onSelect(event);
+  }
+
+  const [currencies, setCurrencies] = useState([])
+
+  useEffect( () => {
+    const getAllCurrencies  = async () => {
+      try{
+        const respuesta = await axios.get(`${apiUrl}/currency/getAll`);    
+        const datos = await respuesta.data;
+        setCurrencies(datos);
+      }catch(error){
+        console.error("Error while get currencies from database: " + error)
+      }
+    };
+    
+    getAllCurrencies();
+  }, [])
+  
+  return (
+    <div className={className}>
+        <label className="flex text-sm font-medium leading-6 text-gray-300">{textLabel}</label>
+        <select
+        name="currency"
+        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        onChange={handleChange}
+        >
+          <option>Select Currency</option>
+          {currencies.map((currency) => (
+            <option key={'currency'+currency.id} value={currency.value}>{currency.name}</option>
+          ))}    
+        </select>
+    </div>
+  )
+}
